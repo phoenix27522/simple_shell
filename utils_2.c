19ec9449c6db8a0fc0b82bc9ecf06a4aba884f92
Nahom kiflu <kiflunahom1994@gmail.com>
2023-09-10 21:15:41 +0300

    implementing our owen getenv and adding utils_2.c

---
+++ utils_2.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+	if (n == 0)
+		return (0);
+	while (*s1 && *s2 && n > 0)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+
+		s1++;
+		s2++;
+		n--;
+	}
+	if (n == 0)
+                return (0);
+
+	return (*s1 - *s2);
+}
